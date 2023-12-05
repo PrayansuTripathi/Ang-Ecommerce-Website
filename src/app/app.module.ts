2c2495f767615c660705ba2caa1d8164eece2229
prayansu <prayansutripathi@gmail.com>
2023-12-06 00:48:19 +0530

    home page done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './layouts/header/header.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ShopComponent } from './pages/shop/shop.component';
+import { BlogComponent } from './pages/blog/blog.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { FeatureComponent } from './layouts/feature/feature.component';
+import { ProductComponent } from './layouts/product/product.component';
+import { SubscriptionComponent } from './pages/subscription/subscription.component';
+import { FooterComponent } from './layouts/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    ShopComponent,
+    BlogComponent,
+    AboutComponent,
+    ContactComponent,
+    FeatureComponent,
+    ProductComponent,
+    SubscriptionComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/pages/home/home.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent {
+product=[1,3,54,43,3,4];
+}
